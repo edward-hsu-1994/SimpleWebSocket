16a0bb6d87ad577d9ffc3c16da780cb054f75c35
徐培堯 <st89382000@gmail.com>
2016-11-05 17:42:06 +0800

    加入許可證與專案連結

--- src/SimpleWebSocket/WebSocketHandler.cs
+++ src/SimpleWebSocket/WebSocketHandler.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SimpleWebSocket {
